7ce2a12d52a6c5558b00d484cb11861b538db0ee
Dave Ejezie <evergreendave1@gmail.com>
2024-12-31 13:31:32 +0000

    feat: create best-gear component to define the business goals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { BestGearComponent } from './best-gear/best-gear.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, HomeComponent],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, HomeComponent, BestGearComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/best-gear/best-gear.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-best-gear',
+  imports: [],
+  templateUrl: './best-gear.component.html',
+  styleUrl: './best-gear.component.scss'
+})
+export class BestGearComponent {
+
+}
