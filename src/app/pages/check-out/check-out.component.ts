103073ea4348c9c2f9f4cc5c70a6597dd3db5324
Dave Ejezie <evergreendave1@gmail.com>
2025-01-16 12:12:36 +0000

    Add back navigation button tp checkout page for better user experince

--- src/app/pages/check-out/check-out.component.ts
+++ src/app/pages/check-out/check-out.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormGroup } from '@angular/forms';
 import { CheckoutFormComponent } from '../../components/forms/checkout-form/checkout-form.component';
 import { SummaryComponent } from '@app/components/cart-summary/cart-summary.component';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-check-out',
@@ -10,11 +11,17 @@ import { SummaryComponent } from '@app/components/cart-summary/cart-summary.comp
   imports: [CommonModule, CheckoutFormComponent, SummaryComponent],
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.scss'],
-  schemas: [NO_ERRORS_SCHEMA],
 })
 export class CheckOutComponent implements OnInit {
+  constructor(private location: Location) {} // Add constructor
+
   checkoutForm!: FormGroup;
   formStatus: boolean = false;
+
+
+  goBack(): void {
+    this.location.back();
+  }
 
   ngOnInit(): void {}
 
