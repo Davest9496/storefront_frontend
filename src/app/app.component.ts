dc333be227708d9136aaef19db4a27b43cab9d6c
Dave Ejezie <evergreendave1@gmail.com>
2025-01-16 13:47:45 +0000

    feat: Dynamically render best gear component and selected routes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BestGearComponent } from './components/best-gear/best-gear.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    HeaderComponent,
-    FooterComponent,
-    RouterOutlet,
-    BestGearComponent,
-  ],
+  imports: [CommonModule, HeaderComponent, FooterComponent, RouterOutlet, BestGearComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'storefront';
+
+  showBestGear: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.showBestGear = !event.url.includes('/checkout');
+      });
+  }
 }
