43da25f4389815593c493b2d3224eb2aa4a09f9d
Dave Ejezie <evergreendave1@gmail.com>
2025-02-05 10:57:29 +0000

    update product component to use httpClient service

--- src/app/pages/home/products/products.component.ts
+++ src/app/pages/home/products/products.component.ts
@@ -1,47 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { ButtonComponent } from '../../../components/button/button.component';
+
+interface Product {
+  id: string;
+  name: string;
+  description?: string;
+  image: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  pattern?: string;
+  class: 'product_featured' | 'product_secondary' | 'product_tertiary';
+}
 
 @Component({
   selector: 'app-products',
-  imports: [ButtonComponent, RouterModule],
+  standalone: true,
+  imports: [CommonModule, ButtonComponent, RouterModule],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
-  products = [
-    {
-      id: 'zx9',
-      name: 'ZX9 SPEAKER',
-      description:
-        'Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.',
-      image: {
-        mobile: 'assets/home/mobile/image-speaker-zx9.png',
-        tablet: 'assets/home/tablet/image-speaker-zx9.png',
-        desktop: 'assets/home/desktop/image-speaker-zx9.png',
-      },
-      pattern: 'assets/home/desktop/pattern-circles.svg',
-      class: 'product_featured',
-    },
-    {
-      id: 'zx7',
-      name: 'ZX7 SPEAKER',
-      image: {
-        mobile: 'assets/home/mobile/image-speaker-zx7.jpg',
-        tablet: 'assets/home/tablet/image-speaker-zx7.jpg',
-        desktop: 'assets/home/desktop/image-speaker-zx7.jpg',
-      },
-      class: 'product_secondary',
-    },
-    {
-      id: 'yx1',
-      name: 'YX1 EARPHONES',
-      image: {
-        mobile: 'assets/home/mobile/image-earphones-yx1.jpg',
-        tablet: 'assets/home/tablet/image-earphones-yx1.jpg',
-        desktop: 'assets/home/desktop/image-earphones-yx1.jpg',
-      },
-      class: 'product_tertiary',
-    },
-  ];
+export class ProductsComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<{ products: Product[] }>('assets/data/products.json')
+      .subscribe((data) => {
+        this.products = data.products;
+      });
+  }
 }
