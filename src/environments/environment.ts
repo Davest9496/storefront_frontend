f01d84b149e64b450940afd906535df650bdd6d3
Dave Ejezie <evergreendave1@gmail.com>
2025-04-14 16:06:38 +0100

    serve static images from s3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:8080/api',
-  assetUrl: '/assets/', // Base URL for assets in development
+  assetUrl: 'https://storefront-images-058264347310.s3.eu-west-2.amazonaws.', // Base URL for assets in development
   debug: true
 };
