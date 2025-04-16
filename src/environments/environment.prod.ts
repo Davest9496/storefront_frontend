413a655d5fcb7f0da7deaea0066d8002e97d8d0e
Dave Ejezie <evergreendave1@gmail.com>
2025-04-16 17:30:18 +0100

    Add apiUrl aws server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 // This file configures production environment variables
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api-url.com/api', // Replace with actual production API URL
+  apiUrl: 'https://olzduu3w3h.execute-api.eu-west-2.amazonaws.com/dev/api', // Replace with actual production API URL
   assetUrl:
-    'https://storefront-images-058264347310.s3.eu-west-2.amazonaws.com/assets/', // Replace with actual S3 bucket URL
+    'https://storefront-images-058264347310.s3.eu-west-2.amazonaws.com/assets/',
   debug: false,
 };
