3589aa96802b19e8e355b7365ea0c88e264d5039
Dave Ejezie <evergreendave1@gmail.com>
2025-04-17 11:16:01 +0100

    update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // This file configures production environment variables
 export const environment = {
   production: true,
-  apiUrl: 'https://olzduu3w3h.execute-api.eu-west-2.amazonaws.com/dev/api', // Replace with actual production API URL
+  apiUrl: 'https://olzduu3w3h.execute-api.eu-west-2.amazonaws.com/dev/api',
   assetUrl:
     'https://storefront-images-058264347310.s3.eu-west-2.amazonaws.com/assets/',
   debug: false,
